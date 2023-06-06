3fcca12998e437033ae083779742c446d56f8a3f
wgz <2111377105@qq.com>
2023-06-06 17:03:13 +0800

    j

--- src/编程思想/Discount.java
+++ src/编程思想/Discount.java
@@ -2,24 +2,95 @@ package 编程思想;
 
 import java.util.Scanner;
 
-//淡旺季机票打折，假设旺季打8折，淡季打5折，所以得先有一个价格price
-//涉及月份应该使用switch
+/*
+ * 需求：机票价格按照季节（淡旺季）
+ * 舱位（头等舱、商务舱、经济舱）
+ * 旺季（5-10月），头等舱9折，商务舱8.5折，经济舱8折
+ * 淡季（11-4）头等舱7折，商务舱65折，经济舱6折
+ * 如果要得到最后的价格，可以将计算价格的过程封装成一个方法
+ * 通过用户进行输入，告知信息
+ * */
 public class Discount {
     public static void main(String[] args) {
-        double price = 800;
+//        double price1 = 800;
+//        double price2 = 700;
+//        double price3 = 600;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请用户分别输入机票月份、舱位");
+//        int month = scanner.nextInt();
+//        String ship = scanner.next();
+//        switch (month){
+//            case 5:
+//            case 6:
+//            case 7:
+//                if(ship.equals("头等舱")) {
+//                    System.out.println("选择头等舱，在旺季打9折后为： " + price1 * 0.9);
+//                }else if(ship.equals("商务舱")){
+//                    System.out.println("选择商务舱，在旺季打85折后为：  "+price2*0.85);
+//                }else if(ship.equals("经济舱")){
+//                    System.out.println("选择经济舱，在旺季打85折后为：  "+price3*0.8);
+//                }
+//                break;
+//            case 11:
+//            case 12:
+//            case 1:
+//                System.out.println("打五折后为： "+price1*0.5);
+//                break;
+//        }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请用户输入机票月份");
+        System.out.println("请输入机票原价");
+        double price = scanner.nextDouble();
+        System.out.println("请输入月份");
         int month = scanner.nextInt();
-        switch (month){
-            case 1:
-            case 2:
-            case 3:
-                System.out.println("打8折后为： "+price*0.8);
-                break;
-            case 4:
-            case 5:
-            case 6:
-                System.out.println("打五折后为： "+price*0.5);
+        System.out.println("请选择舱位：1、头等舱 2、商务舱 3、经济舱");
+        int type = scanner.nextInt();
+        double finalPrice = work(price, month, type);
+        if (finalPrice != -1) {
+            System.out.println("最终价格为：  " + finalPrice);
         }
+
+    }
+
+    public static double work(double price, int month, int type) {
+        double finalPrice = price;
+        boolean loop = true;
+        //先做错误的情况
+        if (price < 0) {
+            System.out.println("机票原价输入错误");
+            return -1;
+        }
+        if (month < 1 || month > 12) {
+            System.out.println("月份输入错误");
+            return -1;
+        }
+        if (type > 3 || type < 1) {
+            System.out.println("舱位输入错误");
+            return -1;
+        } else {
+            switch (type) {
+                case 1:
+                    if (month >= 5 && month <= 10) {
+                        finalPrice *= 0.9;
+                    } else {
+                        finalPrice *= 0.7;
+                    }
+                    break;
+                case 2:
+                    if (month >= 5 && month <= 10) {
+                        finalPrice *= 0.85;
+                    } else {
+                        finalPrice *= 0.65;
+                    }
+                    break;
+                case 3:
+                    if (month >= 5 && month <= 10) {
+                        finalPrice *= 0.8;
+                    } else {
+                        finalPrice *= 0.6;
+                    }
+                    break;
+            }
+        }
+        return finalPrice;
     }
 }
--- src/编程思想/Judging.java
+++ src/编程思想/Judging.java
@@ -1,5 +1,9 @@
 package 编程思想;
-
+/*
+* 需求
+* 有N名评委打分
+* 选手最终得分：去掉最高分和最低分的平均分
+ */
 import java.util.Scanner;
 
 //评委给选手打分，去掉最高分和最低分的平均分
@@ -9,17 +13,28 @@ import java.util.Scanner;
 // 如果是用arr[0]一个一个比较的话，代码更加冗余，比完max，又要用arr[1]一个一个比较min
 public class Judging {
     public static void main(String[] args) {
+        double []score = arr(5);
+        double avg = calAvg(score);
+        System.out.println("平均分： "+avg);
+    }
+    public static double []arr(int num){//只负责录入分数
         Scanner scanner = new Scanner(System.in);
-        double [] arr = new double[5];
-        double temp = 0;
+        double []arr = new double[num];
         double total = 0;
-//        double max = 0;
-        int times = 0;
         for (int i = 0; i < arr.length; i++) {
             System.out.println("请评委打分： ");
             arr[i] = scanner.nextDouble();
-            times++;
         }
+        return arr;
+    }
+    public static double calAvg(double []score){
+        double total = calTotal(score);
+        double avg = total/(score.length-2);
+        return avg;
+    }
+    public static double calTotal(double []arr){
+        double total = 0;
+        double temp = 0;
         for (int j = 0; j < arr.length-1; j++) {
             for (int i = 0; i < arr.length-1-j; i++) {
                 if(arr[i]>arr[i+1]){
@@ -33,9 +48,10 @@ public class Judging {
             if(i == 0||i ==arr.length-1){
                 continue;
             }else {
-                 total += arr[i];
+                total += arr[i];
             }
         }
-        System.out.println("评委打出的平均分是：  "+total/times);
+        return total;
     }
+
 }
